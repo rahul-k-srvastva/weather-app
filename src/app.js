6a47a483a2190a65f5b43b0a6992117a65c21baf
rahul-k-srvastva <rahul.k.srvastva@gmail.com>
2020-09-10 23:27:57 +0530

    Setup app for heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode');
 const foreCast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('Listening at port 3000');
+app.listen(port, () => {
+	console.log(`Listening at port ${port}`);
 });
